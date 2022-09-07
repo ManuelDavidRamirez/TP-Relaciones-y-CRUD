f4fbaadc723a38aa19d32ba55bb96097f7ca518e
ManuelDavidRamirez <Manuyagami95@gmail.com>
2022-09-07 18:09:06 -0300

    TP - Relaciones y CRUD

---
+++ src/controllers/actorsController.js
@@ -0,0 +1,26 @@
+const db = require('../database/models');
+const sequelize = db.sequelize;
+
+
+const actorsController = {
+    'list': (req, res) => {
+        db.Actor.findAll({
+            include : [
+                {
+                    association : "movies",
+                    include : [
+                        {
+                            association : "genre",
+                            attributes : ["name"],
+                        }
+                    ]
+                }
+            ]
+        })
+            .then(actors => {
+                return res.send(actors)
+            })
+    }
+}
+
+module.exports = actorsController;
